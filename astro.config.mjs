409ebf774580afa0fe5a42e5a0bd90bbb43d7d71
Brian Via <brian.via.dev@gmail.com>
2022-11-10 14:14:32 -0500

    add tailwind?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import image from "@astrojs/image";
 // https://astro.build/config
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), image()],
+  integrations: [preact(), image(), tailwind()],
   site: `http://astro.build`
 });
