9f8a3165bd45fa65b9d5704e6bf9ad71ec649e53
Brian Via <brian.via.dev@gmail.com>
2024-09-11 17:12:09 -0400

    add robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
-  site: `https://brianvia.blog`
+  integrations: [tailwind(), sitemap(), robotsTxt()],
+  site: `https://brianvia.blog`,
 });
