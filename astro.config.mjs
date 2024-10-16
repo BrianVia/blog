cd22494befc105c5dc49b8878577a66c9b5a1e38
Brian Via <brian.via.dev@gmail.com>
2024-10-16 13:41:44 -0400

    latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,12 @@ import robotsTxt from "astro-robots-txt";
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: `https://brianvia.blog`,
+  trailingSlash: "never",
+  markdown: {
+    gfm: true,
+    breaks: false,
+    pedantic: false,
+    tables: true,
+    sanitize: false,
+  },
 });
